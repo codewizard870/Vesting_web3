edfd6edb7a0c44356332d655accfa77d0503dc83
Abhinav Das <adas@h0mm.local>
2022-05-09 16:56:33 +0530

    changing copy on claims page and staking page

--- src/pages/Claim/index.tsx
+++ src/pages/Claim/index.tsx
@@ -1,28 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react'
-import { ClaimList } from './ClaimList'
+import React from 'react';
+import { ClaimList } from './ClaimList';
 
 export const Claim = () => {
-
   return (
     <div className="w-full flex justify-center py-10 px-6 lg:px-8 xl:px-16 2xl:px-[124px]">
-      <div className='w-full max-w-[1620px] flex flex-col items-center gap-16 md:gap-20'>
-        <div className='w-full'>
-          <div className='mt-16 mb-4 md:hidden w-full flex justify-center'>
+      <div className="w-full max-w-[1620px] flex flex-col items-center gap-16 md:gap-20">
+        <div className="w-full">
+          <div className="mt-16 mb-4 md:hidden w-full flex justify-center">
             <div>
-              <div className='text-[26px] font-semibold text-[#051C42] uppercase'>Claiming</div>
-              <div className='bg-[#3FBCE9] h-0.5 w-full'></div>
+              <div className="text-[26px] font-semibold text-[#051C42] uppercase">Claiming</div>
+              <div className="bg-[#3FBCE9] h-0.5 w-full"></div>
             </div>
           </div>
-          <div className='hidden md:block'>
-            <div className='text-[28px] text-[#0A208F] font-medium uppercase'>Claiming</div>
+          <div className="hidden md:block">
+            <div className="text-[28px] text-[#0A208F] font-medium uppercase">Claiming</div>
           </div>
-          <div className='text-[16px] md:text-[20px] text-center md:text-left font-regular text-[#3F3F3F] mt-2'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <div className="text-[16px] md:text-[20px] text-center md:text-left font-regular text-[#3F3F3F] mt-2">
+            If you have invested in FLUID, then you’re in the right place! FLUID’s claims portal
+            will help visualize the total amount you have invested, at what round, and a timeline of
+            vested available to claim versus total across the vested period. It will also allow you
+            to claim your tokens into your preferred wallet.
           </div>
         </div>
         <ClaimList />
       </div>
     </div>
-  )
-}
+  );
+};
--- src/pages/Staking/StakingView.tsx
+++ src/pages/Staking/StakingView.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
-import { Box, makeStyles } from '@material-ui/core'
-import clsx from 'clsx'
-import { useStaking } from 'contexts'
-import { StakingPool } from './StakingPool'
-import CircularProgress from '@material-ui/core/CircularProgress'
+import React, { useEffect, useState } from 'react';
+import { Box, makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
+import { useStaking } from 'contexts';
+import { StakingPool } from './StakingPool';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -20,39 +20,43 @@ const useStyles = makeStyles(() => ({
   progress: {
     width: '100%',
     display: 'flex',
-    justifyContent: 'center'
-  }
-}))
+    justifyContent: 'center',
+  },
+}));
 
 export const StakingView = () => {
-  const classes = useStyles()
-  const { poolList, updatePoolList } = useStaking()
-  const [isLoading, setIsLoading] = useState(true)
+  const classes = useStyles();
+  const { poolList, updatePoolList } = useStaking();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetch = async () => {
-      await updatePoolList()
-      setIsLoading(false)
-    }
-    fetch()
-  }, [])
+      await updatePoolList();
+      setIsLoading(false);
+    };
+    fetch();
+  }, []);
 
   return (
-    <div className='w-full rounded-[20px] bg-white shadow-xl p-6 lg:px-16 lg:py-7'>
-      <div className='w-full'>
-        <div className='text-[28px] text-[#0A208F] font-medium uppercase'>STAKING/LP</div>
-        <div className='text-[16px] md:text-[20px] font-regular text-[#3F3F3F] mt-2'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    <div className="w-full rounded-[20px] bg-white shadow-xl p-6 lg:px-16 lg:py-7">
+      <div className="w-full">
+        <div className="text-[28px] text-[#0A208F] font-medium uppercase">STAKING/LP</div>
+        <div className="text-[16px] md:text-[20px] font-regular text-[#3F3F3F] mt-2">
+          Staking with FLUID could not be more accessible and beneficial. By Staking $FLD, you are
+          instantly rewarded with reduced fees and earn rewards compounded daily into more $FLD. The
+          rewards earned is dynamic and is based on supply versus demand.
         </div>
       </div>
-      {!isLoading ?
-        <div className='flex flex-col xl:flex-row justify-center items-center gap-10 xl:gap-[110px] mt-10 mb-8'>
+      {!isLoading ? (
+        <div className="flex flex-col xl:flex-row justify-center items-center gap-10 xl:gap-[110px] mt-10 mb-8">
           {poolList.map((poolInfo, pid) => (
             <StakingPool poolInfo={poolInfo} pid={pid} key={pid} />
           ))}
-        </div> :
+        </div>
+      ) : (
         <div className={classes.progress}>
           <CircularProgress />
-        </div>}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
